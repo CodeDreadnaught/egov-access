277b4284a7c87db310bf80df4b29ebc419600c7f
Akinsoyinu Olumide <olumideakinsoyinu@gmail.com>
2025-03-21 15:24:12 +0100

    created eGov Access Version 1.0

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "eGov Access",
+  description:
+    "eGov Access is your go-to platform for seamless access to Nigerian Government services and efficient  communication with the Plateau State Government.",
+  icons: "/favicon.ico",
+  manifest: "/manifest.json",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
